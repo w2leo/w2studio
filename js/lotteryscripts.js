57752d6bad14db37740a5e9ad6fa054e78c53dcc
MikhailLeonov <mikhail.leonov@ics-it.ru>
2023-03-20 12:02:29 +0300

    fix email validation

--- js/lotteryscripts.js
+++ js/lotteryscripts.js
@@ -1,22 +1,17 @@
 // Attach a click event listener to the submit Lottery button
-document.getElementById('submitLottery').addEventListener('click', function () {
+document.getElementById('submitLottery').addEventListener('click', function (event) {
 	event.preventDefault(); // Prevent the default form submission behavior
-
-
+	// Validate email
 	var form = document.querySelector('.needs-validation');
 	if (form.checkValidity() === false) {
 	  event.stopPropagation();
 	  form.classList.add('was-validated');
 	  return;
 	}
-
+	// Clear server message
+	document.getElementById('lotteryMessage').textContent = '';
 	// Get the user's email address from the input field
 	var userEmail = document.getElementById('inputEmail').value;
-
-	// If the email is valid, close the Bootstrap popup and send an AJAX request to the PHP script
-	$('#emailHelp').removeClass('text-danger').text("We'll never share your email with anyone else.");
-
-	// Send an AJAX request to the PHP script
 	var xhr = new XMLHttpRequest();
 	xhr.open('POST', '/');
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
@@ -27,11 +22,6 @@ document.getElementById('submitLottery').addEventListener('click', function () {
 	};
 	xhr.send('userEmail=' + userEmail);
 });
-
-function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
 
 // Close video on modal closing
 function stopVideoOnModalClose(videoId) {
